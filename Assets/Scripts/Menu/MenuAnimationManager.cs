5d858750e664b1cd40168536ccf26c08e121d3b5
rdelnez <rdelnez@gmail.com>
2017-10-17 16:45:26 +1100

    Done Menu Functions
    Buttons working and lighting up

--- Assets/Scripts/Menu/MenuAnimationManager.cs
+++ Assets/Scripts/Menu/MenuAnimationManager.cs
@@ -2,8 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class MenuAnimationManager : MonoBehaviour {
+
+	public List<Image> listButtonImage;
+	public Image shopObject;
+	public Image highObject;
+	public Image racingObject;
+	public Image shapesObject;
+	public Image animalsObject;
+	public Image creditsObject;
+
+	public List<Sprite> listButtonSprite;
+	public Sprite shopSpriteOn;
+	public Sprite highSpriteOn;
+	public Sprite racingSpriteOn;
+	public Sprite shapesSpriteOn;
+	public Sprite animalsSpriteOn;
+	public Sprite creditsSpriteOn;
+
+	public List<Sprite> listButtonSpriteOff;
+	public Sprite shopSpriteOff;
+	public Sprite highSpriteOff;
+	public Sprite racingSpriteOff;
+	public Sprite shapesSpriteOff;
+	public Sprite animalsSpriteOff;
+	public Sprite creditsSpriteOff;
 
 	public Vector3 shopRotation;
 	public Vector3 highRotation;
@@ -11,85 +36,148 @@ public class MenuAnimationManager : MonoBehaviour {
 	public Vector3 shapesRotation;
 	public Vector3 animalsRotation;
 	public Vector3 creditsRotation;
+	public float speedRotation;
 
-	public bool isShopAnimating;
-	public bool isHighAnimating;
+	public Quaternion test;
+	public bool isAnimatingMenu;
 
 	public GameObject world;
 	
 	// Use this for initialization
 	void Start () {
-		shopRotation = new Vector3(243f, 0f, 0f);
-		highRotation = new Vector3(333f, 0f, 0f);
-		racingRotation = new Vector3(153f, 0f, 0f);
-
-		isShopAnimating = false;
-		isHighAnimating = false;
-
+		InitializeVariables();
 	}
-	
+		
 	// Update is called once per frame
 	void Update () {
 
 	}
 
-	public void RotateShop() {
-		if (isShopAnimating == false || isHighAnimating == false)
-			StartCoroutine(RotateToShopIENumerator());
+	public void InitializeVariables() {
+
+		listButtonImage = new List<Image>();
+		listButtonImage.Add(shopObject);
+		listButtonImage.Add(highObject);
+		listButtonImage.Add(racingObject);
+		listButtonImage.Add(shapesObject);
+		listButtonImage.Add(animalsObject);
+		listButtonImage.Add(creditsObject);
+
+		listButtonSprite = new List<Sprite>();
+		listButtonSprite.Add(shopSpriteOn);
+		listButtonSprite.Add(highSpriteOn);
+		listButtonSprite.Add(racingSpriteOn);
+		listButtonSprite.Add(shapesSpriteOn);
+		listButtonSprite.Add(animalsSpriteOn);
+		listButtonSprite.Add(creditsSpriteOn);
+
+		listButtonSpriteOff = new List<Sprite>();
+		listButtonSpriteOff.Add(shopSpriteOff);
+		listButtonSpriteOff.Add(highSpriteOff);
+		listButtonSpriteOff.Add(racingSpriteOff);
+		listButtonSpriteOff.Add(shapesSpriteOff);
+		listButtonSpriteOff.Add(animalsSpriteOff);
+		listButtonSpriteOff.Add(creditsSpriteOff);
+
+		shopRotation = new Vector3(243f, 0f, 0f);
+		highRotation = new Vector3(333f, 0f, 0f);
+		racingRotation = new Vector3(153f, 0f, 0f);
+		creditsRotation = new Vector3(63f, 0f, 0f);
+		animalsRotation = new Vector3(0f, 90f, 63f);
+		shapesRotation = new Vector3(0f, 90f, 253f);
+
+		speedRotation = 120.0f;
+		isAnimatingMenu = false;
+
+
 	}
 
-	IEnumerator RotateToShopIENumerator() {
-		isShopAnimating = true;
-		Quaternion test = Quaternion.Euler(shopRotation);
-		while (isShopAnimating) {
-			if (Vector3.Distance(world.transform.eulerAngles, shopRotation) > 0.5f) {
-
-				
-				world.transform.rotation = Quaternion.Slerp(world.transform.rotation, test, 0.05f);
-			}
-			else{
-				world.transform.eulerAngles = shopRotation;
-				isShopAnimating = false;
-			}
-			yield return new WaitForSeconds(0.01f);
+	public void RotateShop() {
+		if (isAnimatingMenu == false)
+		{
+			StartCoroutine(RotateWorldIENumerator(shopRotation));
 		}
 	}
 
+	
+
 	public void RotateHigh() {
-		if(isHighAnimating == false || isHighAnimating == false)
-			StartCoroutine(RotateToHighIENumerator());
+		if (isAnimatingMenu == false)
+		{
+			StartCoroutine(RotateWorldIENumerator(highRotation));
+		}
 	}
 
-	IEnumerator RotateToHighIENumerator()
-	{
-		isHighAnimating = true;
-		Quaternion test = Quaternion.Euler(highRotation);
-		while (isHighAnimating)
+
+	public void RotateRacing() {
+		if (isAnimatingMenu == false)
 		{
-			if (Vector3.Distance(world.transform.eulerAngles, highRotation) > 0.5f)
+			StartCoroutine(RotateWorldIENumerator(racingRotation));
+		}
+	}
+
+	public void RotateCredits()
+	{
+		if (isAnimatingMenu == false)
+		{
+			StartCoroutine(RotateWorldIENumerator(creditsRotation));
+		}
+	}
+
+	public void RotateAnimals()
+	{
+		if (isAnimatingMenu == false)
+		{
+			StartCoroutine(RotateWorldIENumerator(animalsRotation));
+		}
+	}
+
+	public void RotateShapes()
+	{
+		if (isAnimatingMenu == false)
+		{
+			StartCoroutine(RotateWorldIENumerator(shapesRotation));
+		}
+	}
+
+	public void EnableDisableMenuButton(int _tempIndex) {
+
+
+		Debug.Log(listButtonImage.Count);
+
+		for (int x=0; x<listButtonImage.Count; x++) {
+			listButtonImage[x].sprite = listButtonSpriteOff[x];
+		}
+
+		listButtonImage[_tempIndex].sprite = listButtonSprite[_tempIndex] as Sprite;
+
+	
+	}
+
+
+	IEnumerator RotateWorldIENumerator(Vector3 _landPos) {
+
+		isAnimatingMenu = true;
+		Quaternion _landPosQuaternion = Quaternion.Euler(_landPos);
+		while (isAnimatingMenu) {
+
+			yield return new WaitForSeconds(0.01f);
+
+			if (Quaternion.Angle(world.transform.rotation, _landPosQuaternion) > 1.5f)
 			{
+				
 
-
-				world.transform.rotation = Quaternion.Slerp(world.transform.rotation, test, 0.05f);
+				world.transform.rotation = Quaternion.RotateTowards(world.transform.rotation, _landPosQuaternion, speedRotation*Time.deltaTime);
 			}
 			else
 			{
-				world.transform.eulerAngles = highRotation;
-				isHighAnimating = false;
+				world.transform.eulerAngles = _landPos;
+				isAnimatingMenu = false;
 			}
-			yield return new WaitForSeconds(0.01f);
+
+			
 		}
 	}
 
-	/*--
-	if(Vector3.Distance(sanctuary.transform.eulerAngles, sancTempVectorRot)>0.01f){
-				sanctuary.transform.eulerAngles = Vector3.Lerp(sanctuary.transform.eulerAngles, sancTempVectorRot, 0.1f);
-				sanctuary.transform.localPosition = Vector3.Lerp(sanctuary.transform.localPosition, sancTempVector, 0.1f);
-			}
-			else{
-				sanctuary.transform.eulerAngles = sancTempVectorRot;
-				PlaySound();
-			isAnimating = false;
-			}
-			--*/
+
 }
