e24faa8cef21fbeab2f1f829d7932c90f8935522
rdelnez <rdelnez@gmail.com>
2017-11-09 14:41:49 +1100

    Create Static SVM and SOUNDMANAGER
    on all scenes

---
+++ Assets/Scripts/Static/SVM_Script.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SVM_Script : MonoBehaviour
+{
+
+	public static string gameDifficulty;
+	
+	public static SVM_Script Instance
+	{
+		get;
+		set;
+	}
+
+	public GameObject canvas;
+	private GameObject loadingScreen;
+
+	/// <summary>
+	/// Target time to beat in seconds.
+	/// </summary>
+	
+
+	void Awake()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+		
+	//	SceneManager.sceneLoaded += SceneLoadListener;
+	//	SetUpLoadingScreen();           //Establish the loading screen and attach it to the canvas while fixing strange value artifacts
+	//	InitializeSavedVariables();     //Initialize variables that needs to be saved when APP is closed
+	//	LoadSavedVariables();           //Load the variables from Playerprefs
+	
+	}
+
+	void SetUpLoadingScreen()
+	{
+		canvas = GameObject.FindGameObjectWithTag("Canvas");
+		loadingScreen = Instantiate(Resources.Load("prefabs/LoadingScreen")) as GameObject;
+		loadingScreen.name = "Loading Screen";
+		loadingScreen.GetComponent<RectTransform>().anchorMin = new Vector2(0.5f, 0.5f);
+		loadingScreen.GetComponent<RectTransform>().anchorMax = new Vector2(0.5f, 0.5f);
+		loadingScreen.GetComponent<RectTransform>().sizeDelta = new Vector2(800, 450);
+		loadingScreen.transform.SetParent(canvas.transform);
+		loadingScreen.GetComponent<RectTransform>().localPosition = Vector3.zero;
+		loadingScreen.GetComponent<RectTransform>().localScale = Vector3.one;
+		loadingScreen.SetActive(false);
+	}
+
+
+	void SceneLoadListener(Scene scene, LoadSceneMode loadMode)
+	{
+	
+	
+		SetUpLoadingScreen();
+	}
+
+	/*--
+	public void InitializeSavedVariables()
+	{
+		if (!PlayerPrefs.HasKey("EE_advance"))
+		{
+			PlayerPrefs.SetInt("EE_advance", 0);
+		}
+		if (!PlayerPrefs.HasKey("EE_expert"))
+		{
+			PlayerPrefs.SetInt("EE_expert", 0);
+		}
+		if (!PlayerPrefs.HasKey("Instructions_Dismissed"))
+		{
+			PlayerPrefs.SetInt("Instructions_Dismissed", 0);
+		}
+	}
+
+	public void LoadSavedVariables()
+	{
+		if (PlayerPrefs.GetInt("EE_advance") == 1)
+		{
+			advanceIsLocked = false;
+		}
+		else
+		{
+			advanceIsLocked = true;
+		}
+
+		if (PlayerPrefs.GetInt("EE_expert") == 1)
+		{
+			expertIsLocked = false;
+		}
+		else
+		{
+			expertIsLocked = true;
+		}
+		if (PlayerPrefs.GetInt("Instructions_Dismissed") == 1)
+		{
+			InstructionSeen = true;
+		}
+		else
+		{
+			InstructionSeen = false;
+		}
+	}
+
+	public void LoadLevel(string levelName)
+	{
+		if (!loadingScreen)
+		{
+			Debug.LogError("Loading Screen has not been set up");
+			return;
+		}
+		loadingScreen.SetActive(true);
+		StartCoroutine(LevelLoader(levelName));
+	}
+
+	IEnumerator LevelLoader(string levelName)
+	{
+		yield return new WaitForSeconds(0.5f);
+		AsyncOperation async = SceneManager.LoadSceneAsync(levelName);
+		yield return async;
+	}
+
+	--*/
+}
---
+++ Assets/Scripts/Static/SoundManagerScript.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SoundManagerScript : MonoBehaviour
+{
+
+	//public AudioSource BGMusic_Player;
+	public AudioSource FX_Player;
+	public AudioSource BG_FX_Player;
+	public AudioSource BG_FX_Loop_Player;
+
+	private bool isMute;
+
+	public Sprite audioSprite;
+	public Sprite audioMuteSprite;
+	private GameObject audioButton;
+	private Image audioButtonImage;
+
+	public static SoundManagerScript Instance
+	{
+		get;
+		set;
+	}
+	// Use this for initialization
+
+	void Awake()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void GetMuteButton()
+	{
+		audioButton = GameObject.FindGameObjectWithTag("MuteButton");
+		audioButtonImage = audioButton.GetComponent<Image>();
+	}
+
+	public void Play_Menu_SFX()
+	{
+		FX_Player.clip = Resources.Load<AudioClip>("SoundEffects/SFX/hit");
+		FX_Player.Play();
+		//FX_Player.PlayOneShot (Resources.Load<AudioClip> ("Voices/Set" + setNum + "/" + fileName + ballNum));
+	}
+
+	public void Play_SFX(string tempStringName)
+	{
+		FX_Player.clip = Resources.Load<AudioClip>("SoundEffects/SFX/" + tempStringName);
+		FX_Player.Play();
+		//FX_Player.PlayOneShot (Resources.Load<AudioClip> ("Voices/Set" + setNum + "/" + fileName + ballNum));
+	}
+
+	public void Play_BG_SFX(string tempStringName)
+	{
+		BG_FX_Player.clip = Resources.Load<AudioClip>("SoundEffects/BG_SFX/" + tempStringName);
+		BG_FX_Player.loop = true;
+		BG_FX_Player.Play();
+		//FX_Player.PlayOneShot (Resources.Load<AudioClip> ("Voices/Set" + setNum + "/" + fileName + ballNum));
+	}
+	public void Pause_BG_SFX()
+	{
+		BG_FX_Player.Pause();
+		//FX_Player.PlayOneShot (Resources.Load<AudioClip> ("Voices/Set" + setNum + "/" + fileName + ballNum));
+	}
+	public void Resume_BG_SFX()
+	{
+		BG_FX_Player.UnPause();
+		//FX_Player.PlayOneShot (Resources.Load<AudioClip> ("Voices/Set" + setNum + "/" + fileName + ballNum));
+	}
+
+	public void Play_BG_loop(string tempStringName)
+	{
+		
+		BG_FX_Loop_Player.clip = Resources.Load<AudioClip>("SoundEffects/BG_Loop/" + tempStringName);
+		BG_FX_Loop_Player.loop = true;
+		BG_FX_Loop_Player.Play();
+		//FX_Player.PlayOneShot (Resources.Load<AudioClip> ("Voices/Set" + setNum + "/" + fileName + ballNum));
+	}
+
+	public void Stop_BG_SFX()
+	{
+		BG_FX_Player.Stop();
+		//FX_Player.PlayOneShot (Resources.Load<AudioClip> ("Voices/Set" + setNum + "/" + fileName + ballNum));
+	}
+
+	public void ChangeBGMusic(string tempBGMusic)
+	{
+		FX_Player.clip = Resources.Load<AudioClip>("BGM/" + tempBGMusic);
+		FX_Player.loop = true;
+		FX_Player.Play();
+	}
+
+	public void ButtonAudio()
+	{
+		isMute = !isMute;
+		if (isMute == true)
+		{
+			AudioListener.volume = 0.0f;
+			//audioSpriteRenderer.sprite = audioMuteSprite;
+			audioButtonImage.sprite = audioMuteSprite;
+		}
+		else
+		{
+			AudioListener.volume = 1.0f;
+			audioButtonImage.sprite = audioSprite;
+		}
+	}
+}
