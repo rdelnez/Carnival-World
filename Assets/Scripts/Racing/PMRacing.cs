01d961f7b61b72bf2bcf1a66f8fbac3bf13d6173
Kevinhefford <Kevinhefford@gmail.com>
2017-11-13 14:29:05 +1100

    Functional Script, Animated Sprites.
    Moved sprits.y and snapped positions for loop.
    Animated wheels and added UI buttons "left & Right"

--- Assets/Scripts/Racing/PMRacing.cs
+++ Assets/Scripts/Racing/PMRacing.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PMRacing : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public GameObject MonsterTruck;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    public void MoveRight()
+       
+    {
+
+
+
+    }
+
+    public void MoveLeft()
+
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
--- Assets/Scripts/Racing/TrackRacing.cs
+++ Assets/Scripts/Racing/TrackRacing.cs
@@ -4,11 +4,61 @@ using UnityEngine;
 
 public class TrackRacing : MonoBehaviour {
 
+    public GameObject RacingTrackOne;
+    public GameObject RacingTrackTwo;
+    public int moveToThisY;
+    public float speedY;
+   
+
 	// Use this for initialization
 	void Start () {
-		
+        speedY = -3.0f;
+        moveToThisY = -100;
+        
+
+        StartCoroutine("MoveRaceTrack");
 	}
-	
+
+    IEnumerator MoveRaceTrack()
+
+    {
+        while (transform.position.y > moveToThisY)
+ 
+        {
+
+            RacingTrackOne.transform.Translate(0 * Time.deltaTime, speedY * Time.deltaTime, 0);
+            RacingTrackTwo.transform.Translate(0 * Time.deltaTime, speedY * Time.deltaTime, 0);
+
+            if (RacingTrackOne.transform.position.y <= -23f)
+
+            {
+               
+                RacingTrackOne.transform.position = new Vector3(0, 23, 0);
+                
+                yield return null;
+
+            }
+
+
+
+            if (RacingTrackTwo.transform.position.y <= -23f)
+
+
+            {
+                
+                RacingTrackTwo.transform.position = new Vector3(0, 23, 0);
+                
+                yield return null;
+            }
+
+            yield return null;
+
+        }
+
+      
+    }
+
+
 	// Update is called once per frame
 	void Update () {
 		
