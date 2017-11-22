180d3f8bda15cba49e9d2b45554cb7842d214f78
Kevinhefford <Kevinhefford@gmail.com>
2017-11-22 16:29:44 +1100

    Added references and empty object GM, PM altered code.
    Added references and empty object GM, PM altered code.

--- Assets/Scripts/Racing/GMRacing.cs
+++ Assets/Scripts/Racing/GMRacing.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GMRacing : MonoBehaviour {
     public int lives;
@@ -10,6 +11,10 @@ public class GMRacing : MonoBehaviour {
     public GameObject GerryCan;
     public float speedY;
     public int MoveToY;
+    public Text Equation;
+    public int num1;
+    public int num2;
+    
 
 
     // Use this for initialization
@@ -17,28 +22,16 @@ public class GMRacing : MonoBehaviour {
         lives = 5;
         checkpoint = 100;
         score = 0;
-        speedY = -0.5f;
+        speedY = -3f;
         MoveToY = -100;
         StartCoroutine("MoveGameObjects");
-
+        num1 = (Random.Range(1, 9));
+        num2 = (Random.Range(1, 9));
+        Equation = Equation.GetComponent<Text>();
+        Equation.text = (num1 * num2).ToString();
+     
     }
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Pick Up"))
-        {
-            other.gameObject.SetActive(false);
-            checkpoint = checkpoint - 20;
-           score = score - 20;
-            lives = lives - 1;
-
-        }
-       else if (other.gameObject.CompareTag("Pick Up Correct"))
-        {
-           other.gameObject.SetActive(false);
-           checkpoint = checkpoint + 20;
-            score = score + 20;
-        }
-  }
+  
     IEnumerator MoveGameObjects()
 
     {
@@ -47,28 +40,33 @@ public class GMRacing : MonoBehaviour {
         {
 
             GerryCan.transform.Translate(0 * Time.deltaTime, speedY * Time.deltaTime, 0);
-            GerryCan.transform.Translate(0 * Time.deltaTime, speedY * Time.deltaTime, 0);
+            
 
-            if (GerryCan.transform.position.y <= -22.8f)
+            if (GerryCan.transform.position.y <= 2.6f)
 
             {
-
-                GerryCan.transform.position = new Vector3(0, 22.8f, 0);
+                
+                GerryCan.transform.position = new Vector3(0, 32.8f, 0);
                 this.gameObject.SetActive(true);
+                
                 yield return null;
 
             }
-
-
-
-            if (GerryCan.transform.position.y <= -22.8f)
-
-
+           else if (GerryCan.transform.position.y <= 2.6f)
             {
+                
+                GerryCan.gameObject.SetActive(false);
+                checkpoint = checkpoint - 20;
+                score = score - 20;
+                lives = lives - 1;
 
-                GerryCan.transform.position = new Vector3(0, 22.8f, 0);
-                this.gameObject.SetActive(true);
-                yield return null;
+            }
+            else if (GerryCan.transform.position.y <= 2.6f)
+            {
+               
+                GerryCan.gameObject.SetActive(false);
+                checkpoint = checkpoint + 20;
+                score = score + 20;
             }
 
             yield return null;
