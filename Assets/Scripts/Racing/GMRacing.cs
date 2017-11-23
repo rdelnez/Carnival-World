2a18a6407a203624a391e6d8c852c735d4ff4fa5
Kevinhefford <Kevinhefford@gmail.com>
2017-11-23 11:25:17 +1100

    Individualized GerryCan and swapped them to GameObjects.
    Individualized GerryCan and swapped them to GameObjects.

--- Assets/Scripts/Racing/GMRacing.cs
+++ Assets/Scripts/Racing/GMRacing.cs
@@ -9,15 +9,24 @@ public class GMRacing : MonoBehaviour {
     public int checkpoint;
     public GameObject MonsterTruck;
     public GameObject GerryCan;
+    public GameObject GerryCan2;
+    public GameObject GerryCan3;
+
     public float speedY;
     public int MoveToY;
     public Text Equation;
     public int num1;
     public int num2;
-    
+    public Text RightAnswer;
+    public Text WrongAnswer;
 
 
-    // Use this for initialization
+    private void Awake()
+    {
+        
+    }
+
+
     void Start() {
         lives = 5;
         checkpoint = 100;
@@ -40,31 +49,41 @@ public class GMRacing : MonoBehaviour {
         {
 
             GerryCan.transform.Translate(0 * Time.deltaTime, speedY * Time.deltaTime, 0);
-            
+            GerryCan2.transform.Translate(0 * Time.deltaTime, speedY * Time.deltaTime, 0);
+            GerryCan3.transform.Translate(0 * Time.deltaTime, speedY * Time.deltaTime, 0);
 
-            if (GerryCan.transform.position.y <= 2.6f)
+
+            if (GerryCan.transform.position.y <= 2.6f && GerryCan2.transform.position.y <= 2.6f && GerryCan3.transform.position.y <= 2.6f)
 
             {
                 
-                GerryCan.transform.position = new Vector3(0, 32.8f, 0);
-                this.gameObject.SetActive(true);
-                
+                GerryCan.transform.position = new Vector3(0, 32.8f, -4);
+                GerryCan2.transform.position = new Vector3(0, 32.8f, -4);
+                GerryCan3.transform.position = new Vector3(0, 32.8f, -4);
+                GerryCan.gameObject.SetActive(true);
+                GerryCan2.gameObject.SetActive(true);
+                GerryCan3.gameObject.SetActive(true);
+
                 yield return null;
 
             }
-           else if (GerryCan.transform.position.y <= 2.6f)
+           else if (GerryCan.transform.position.y <= 2.6f && GerryCan2.transform.position.y <= 2.6f && GerryCan3.transform.position.y <= 2.6f)
             {
-                
+
                 GerryCan.gameObject.SetActive(false);
+                GerryCan2.gameObject.SetActive(false);
+                GerryCan3.gameObject.SetActive(false);
                 checkpoint = checkpoint - 20;
                 score = score - 20;
                 lives = lives - 1;
 
             }
-            else if (GerryCan.transform.position.y <= 2.6f)
+            else if (GerryCan.transform.position.y <= 2.6f && GerryCan2.transform.position.y <= 2.6f && GerryCan3.transform.position.y <= 2.6f)
             {
-               
+
                 GerryCan.gameObject.SetActive(false);
+                GerryCan2.gameObject.SetActive(false);
+                GerryCan3.gameObject.SetActive(false);
                 checkpoint = checkpoint + 20;
                 score = score + 20;
             }
