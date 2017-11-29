638bbf9d999459b89a412cb7ae8cffb124197a15
Kevinhefford <Kevinhefford@gmail.com>
2017-11-29 15:57:40 +1100

    Empty Animal Object attached with prefab and instantiate script
    Empty Animal Object attached with prefab and instantiate script

---
+++ Assets/Scripts/WhosAtTheZoo/InstantiateAnimals.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InstantiateAnimals : MonoBehaviour {
+    //public GameObject AnimalPrefab;
+    //GameObject AnimalPrefabClone;
+    List<GameObject> prefabList = new List<GameObject>();
+    public GameObject Prefab1;
+    public GameObject Prefab2;
+    public GameObject Prefab3;
+    public GameObject Prefab4;
+    public GameObject Prefab5;
+    public GameObject Prefab6;
+
+
+    // Use this for initialization
+    void Start () {
+        // AnimalPrefabClone = Instantiate(AnimalPrefab, transform.position, Quaternion.identity) as GameObject;
+        prefabList.Add(Prefab1);
+        prefabList.Add(Prefab2);
+        prefabList.Add(Prefab3);
+        prefabList.Add(Prefab4);
+        prefabList.Add(Prefab5);
+        prefabList.Add(Prefab6);
+
+        int prefabIndex = UnityEngine.Random.Range(0, 6);
+        Instantiate(prefabList[prefabIndex]);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
